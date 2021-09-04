15cf73a6f916c722bda89624f6736f81fb562221
alanrr33 <alanrr33@gmail.com>
2021-09-04 14:08:12 -0300

    Revert "deploy"
    This reverts commit 2056a657833ffa5685431e5b8f38dd3c99590ffa.

--- dam/dam/settings/base.py
+++ dam/dam/settings/base.py
@@ -3,8 +3,6 @@ import mimetypes
 from pathlib import Path
 import json
 
-
-
 from django.core.exceptions import ImproperlyConfigured
 
 
@@ -135,6 +133,4 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
 
